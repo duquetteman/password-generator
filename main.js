49a22f0c0969ba97666b708accf4199623f6b645
Patrick Duquette <duquetteman@gmail.com>
2019-12-13 18:12:31 -0600

    Final edits to main.js and added start over button to index.html.

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-// var generatePassEl = document.getElementById("generatePass");
-// generatePassEl.addEventListener("click") {
-	
-// }
-
-var howManyCharacters = parseInt(prompt("How many characters?"))
-
-if (howManyCharacters  < 8 || howManyCharacters > 128){
-	alert("Password must be between 8 and 128 characters. Please refresh the page and start Over.")
+// Verify how many characters long the user wants the password to be.
+var howManyCharacters = parseInt(prompt("How many characters long would you like your password to be? It must be between 8 and 128 characters."))
+// Conditional to set the range of password characters necessary for the page to work. 
+if (howManyCharacters  < 8 || howManyCharacters > 128){;
+	alert("Hey there, fancy pants... password must be between 8 and 128 number long. Press OK to refresh the page and start Over.")
+	location.reload()
 }
+
+
 else {
-var includeSpecialCharacters = confirm("Do you want special characters?")
-var includeLowerCaseLetters = confirm("Do you want upper case letters?")
-var includeUpperCaseLetters = confirm("Do you want lower case letters?")
-var includeNumbers = confirm("Do you want numbers?")
+var includeSpecialCharacters = confirm("Do you want SPECIAL CHARACTERS?")
+var includeLowerCaseLetters = confirm("Do you want LOWER CASE LETTERS?")
+var includeUpperCaseLetters = confirm("Do you want UPPER CASE LETTERS?")
+var includeNumbers = confirm("Do you want NUMBERS?")
 var symbol = "!@#$%^&*(){}[]=<>/,.";
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lower = "abcdefghijklmnopqrstuvwxyz";
@@ -22,9 +21,9 @@ var charSet = "";
 
 
 
+
 function passwordResult() {
-if (includeSpecialCharacters) {
-	// Do I need something here that references the symbol variable? 
+if (includeSpecialCharacters) { 
 	charSet += symbol;
 }
 
@@ -41,7 +40,8 @@ if (includeNumbers) {
 }
 
 if (charSet == ""){
-	alert("Password must include at least one character option. Please refresh the page and make new choices.")
+	alert("Password must include at least one character option. Press OK to refresh the page and start Over.")
+	location.reload();
 }
 
 var userPassword = "";
@@ -50,5 +50,18 @@ userPassword += charSet[Math.floor(Math.random() * charSet.length)];
 	} 	
 document.getElementById('password').value = userPassword;
 }
+
 generateBtn = document.getElementById("generatePass")
 generateBtn.addEventListener("click", passwordResult);
+
+function copyToClipboard() {
+	var copyText = document.getElementById("password");
+	copyText.select();
+	copyText.setSelectionRange(0, 99999);
+	document.execCommand("copy");
+
+alert("Copied the text: " + copyText.value);
+}
+
+copyBtn = document.getElementById("copy");
+copyBtn.addEventListener("click", copyToClipboard);
